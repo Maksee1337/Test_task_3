f69d11643d6984cb0ad09673a6fdb20685f937ac
Maksee1337 <maksee1337@gmail.com>
2024-02-26 21:41:28 +0200

    test tasks

--- 3/Readme.md
+++
@@ -1,3 +0,0 @@
-1. run docker-compose.yaml from database folder
-2. npm install
-3. npm run start:dev
--- 3/src/modules/order/order.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IOrderAttributes {
-  id: number;
-  bookingObjectId: number;
-  name: string;
-  note: string;
-  quantity: number;
-  startDate: Date;
-  endDate: Date;
-}
